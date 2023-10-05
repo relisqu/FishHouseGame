9bb7c43250308634e173fcc5dd850aa2f9ac65cf
Kopeikina Anna <kopkopkop47@gmail.com>
2023-10-06 00:04:27 +0300

    added sword attack

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [RequireComponent(typeof(Animator))]
+    public class PlayerAttack : MonoBehaviour
+    {
+        [SerializeField]private float MainAttackDelay;
+        
+        private Animator _animator;
+        private bool _isAttacking;
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0) && !_isAttacking)
+            {
+                StartCoroutine(StartMainAttack());
+            }
+        }
+
+        public IEnumerator StartMainAttack()
+        {
+            _isAttacking = true;
+            _animator.SetTrigger("MainAttack");
+            yield return new WaitForSeconds(MainAttackDelay);
+            _isAttacking = false;
+        }
+    }
+}
