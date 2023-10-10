3725888136d50835ded6cc92697b93603838dc3f
Ekaterina Urmanova <aleremualeremus@gmail.com>
2023-10-11 02:21:50 +0300

    Update RecipesDescription.cs

--- Assets/Scripts/Scriptable Objects/RecipesDescription.cs
+++ Assets/Scripts/Scriptable Objects/RecipesDescription.cs
@@ -11,27 +11,24 @@ public class RecipesDescription: SerializedScriptableObject
     [field: SerializeField] public List<RecipeData> recipesDescription { get; private set; }
 }
         
-        
-public class Parameters
-{
-    [SerializeField] public Dictionary<string, GameObject> prefabs;
-    [SerializeField] public Dictionary<string, string> parameters;
-}
+
 
 public class RecipeData
 {
     [field: SerializeField] public string name { get; set; }
     [field: SerializeField] public string description { get; set; }
+    [field: SerializeField, Tooltip("In Seconds")] public float TimeToCook { get; set; }
     [field: SerializeField] public List<IngridientData> Ingridients { get; set; }
 
     
+
+
 }
 
 public class IngridientData
 {
-    [field: SerializeField] IngridientType Ingridient;
-    [field: SerializeField] float TimeToCook;
-    [field: SerializeField] int Amount;
+    [field: SerializeField] public IngridientType Ingridient { get; set; }
+    [field: SerializeField] public int Amount { get; set; }
 }
 
 public enum IngridientType {
