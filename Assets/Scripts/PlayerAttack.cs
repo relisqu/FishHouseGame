ebde7351a0fcf13596dfa434b99cd0638444fe65
Kopeikina Anna <kopkopkop47@gmail.com>
2023-10-11 13:44:21 +0300

    Added fire attack

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -9,9 +9,11 @@ namespace DefaultNamespace
     public class PlayerAttack : MonoBehaviour
     {
         [SerializeField]private float MainAttackDelay;
+        [SerializeField]private float OffAttackDelay;
         
         private Animator _animator;
         private bool _isAttacking;
+        [SerializeField] private ParticleSystem FireParticles;
         private void Start()
         {
             _animator = GetComponent<Animator>();
@@ -24,13 +26,26 @@ namespace DefaultNamespace
             {
                 StartCoroutine(StartMainAttack());
             }
+            else if (Input.GetMouseButton(1))
+            {
+                StartCoroutine(StartOffAttack());
+                FireParticles.Play();
+            }
+            
         }
 
         public IEnumerator StartMainAttack()
         {
             _isAttacking = true;
-            _animator.SetTrigger("MainAttack");
+            _animator.SetTrigger("Attack");
             yield return new WaitForSeconds(MainAttackDelay);
+            _isAttacking = false;
+        }
+        public IEnumerator StartOffAttack()
+        {
+            if (_isAttacking) yield return null;
+            _isAttacking = true;
+            yield return new WaitForSeconds(OffAttackDelay);
             _isAttacking = false;
         }
     }
